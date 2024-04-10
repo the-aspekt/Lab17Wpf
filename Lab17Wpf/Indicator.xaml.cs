c18762a86c582ae5b93b9330ac19e176a40100f8
Pavel <sozonovps@gmail.com>
2024-04-10 15:38:49 +0800

    Добавьте файлы проекта.

---
+++ Lab17Wpf/Indicator.xaml.cs
@@ -0,0 +1,128 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab17Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для Indicator.xaml
+    /// </summary>
+    public partial class Indicator : System.Windows.Controls.UserControl
+    {
+
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+
+        static Indicator()
+        {
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(Indicator),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(Indicator),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(Indicator),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(Indicator),
+                 new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<Color>),typeof(Indicator));
+        }
+
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+        public byte Red
+        {
+            get => (byte)GetValue(RedProperty);
+            set => SetValue(RedProperty, value);
+        }
+        public byte Green
+        {
+            get => (byte)GetValue(GreenProperty);
+            set => SetValue(GreenProperty, value);
+        }
+        public byte Blue
+        {
+            get => (byte)GetValue(BlueProperty);
+            set => SetValue(BlueProperty, value);
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender,
+                    DependencyPropertyChangedEventArgs e)
+        {
+            Indicator indicator = (Indicator)sender;
+            Color color = indicator.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            indicator.Color = color;
+        }
+
+        private static void OnColorChanged(DependencyObject sender,
+      DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            Indicator indicator = (Indicator)sender;
+            indicator.Red = newColor.R;
+            indicator.Green = newColor.G;
+            indicator.Blue = newColor.B;
+
+            RoutedPropertyChangedEventArgs<Color> args = new RoutedPropertyChangedEventArgs<Color>(indicator.Color, newColor);
+            args.RoutedEvent = ColorChangedEvent;
+            indicator.RaiseEvent(args);
+
+        }
+
+        public static readonly RoutedEvent ColorChangedEvent;
+
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent, value); }
+        }
+
+        //private static object CoerceMyValue(DependencyObject d, object baseValue)
+        //{
+        //    double v = (double)baseValue;
+        //    if ( v<0)
+        //    {
+        //        return 0;
+        //    }
+        //    else if ( v > 255 )
+        //    {
+        //        return 255;
+        //    }
+        //    else
+        //        return v;
+        //}
+
+        public Indicator()
+        {
+            InitializeComponent();
+        }
+
+        private void redSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
